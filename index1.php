98a8d95db8a9f204285338b212740f1f0285d9a0
wajdinawaj <wajde.nawajaa@gmail.com>
2024-06-02 13:57:35 +0300

    bytello village

---
+++ index1.php
@@ -0,0 +1,185 @@
+ <!-- Design by  Eng : wajde alnawaja
+http://www.facebook.com/wajde alnawaja
+
+Name       : al-nawaja
+
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="rtl">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>wajde alnawaja</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="wrapper">
+
+	<div id="header">
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="#">Home</a></li>
+				<li><a href="#">yatta </a></li>
+				<li><a href="#">Photos</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Links</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+		<!-- end #menu -->
+		<div id="search">
+			<form method="get" action="">
+				<fieldset>
+				<input type="text" name="s" id="search-text" size="15" />
+				<input name="Submit" type="submit" id="search-submit" value="Search" />
+				</fieldset>
+			</form>
+		</div>
+		<!-- end #search -->
+	</div>
+	<!-- end #header -->
+	<div id="logo">
+		<h1><a href="#">Yatta </a></h1>
+		<p><em> design by wajde al-nawaja </em></p>
+	</div>
+	<hr />
+	<!-- end #logo -->
+<!-- end #header-wrapper -->
+
+<div id="page">
+	<div id="content">
+		<div class="post">
+			<h2 class="title">Welcome to yatta </h2>
+			<p class="date">01.23.09</p>
+			<div class="entry">
+				<p>
+I Yatta, my arteries eight communicate with her womb and immoral, Crown heart of spring water under the Aqaba 4 by Btinan (the bride and the garage), and the laying of my grandchildren party Vwadi third ventricle they called Amparty. I Yatta, Mazna three Msagda fifty and my schools forty, lobby Alaty Hospital invitation denominator ... have fun and play in Lofa 5, and I sleep well at night in a description 6 Aaziz and Chrish meadow Dodta embracing Laroussi, safe fever Zeevi and Maina and Pod my head and acetate grandchildren of sound and Lakes and flowered and deaf and Arabic 7 Dad ... Crown rest ......</p>
+</p>
+				
+			</div>
+		</div>
+		<div class="post">
+			<h2 class="title">city's history </h2>
+			<p class="date">01.23.09</p>
+			<div class="entry">
+				<p> هنا النص مثال لنص يمكن استبداله </p>
+					</div>
+		</div>
+		<div class="post">
+			<h2 class="title">Location and size of the city</h2>
+			<p class="date">01.23.09</p>
+			<div class="entry">
+				<p>Palestinian city of Yatta, located in the West Bank, on the south of Hebron, located about 12 kilometers, and away from the city of Jerusalem Hawally 60 kilometers. It is the third largest population increase in Palestine
+?ihdha from the north: the city of Hebron.
+?ihdha from the south: Samu and Tel Sheva.
+?ihdha from the east: the Dead Sea
+?ihdha of the West: a role.</p>
+						</div>
+		</div>
+	</div>
+	<!-- end #content -->
+	<div id="sidebar">
+		<ul>
+			<li>
+				<h2> Yatta facilities</h2>
+				<p>other.etc..</p>
+			</li>
+			<li id="calendar">
+				<h2>Calendar</h2>
+				<div id="calendar_wrap">
+					<table summary="Calendar">
+						<caption>
+						March 2016
+						</caption>
+						<thead>
+							<tr>
+								<th abbr="Monday" scope="col" title="Monday">M</th>
+								<th abbr="Tuesday" scope="col" title="Tuesday">T</th>
+								<th abbr="Wednesday" scope="col" title="Wednesday">W</th>
+								<th abbr="Thursday" scope="col" title="Thursday">T</th>
+								<th abbr="Friday" scope="col" title="Friday">F</th>
+								<th abbr="Saturday" scope="col" title="Saturday">S</th>
+								<th abbr="Sunday" scope="col" title="Sunday">S</th>
+							</tr>
+						</thead>
+						<tfoot>
+							<tr>
+								<td abbr="February" colspan="3" id="prev"><a href="#" title="">&laquo; Feb</a></td>
+								<td class="pad">&nbsp;</td>
+								<td abbr="April" colspan="3" id="next"><a href="#" title="">Apr &raquo;</a></td>
+							</tr>
+						</tfoot>
+						<tbody>
+							<tr>
+								<td colspan="5" class="pad">&nbsp;</td>
+								<td>1</td>
+								<td>2</td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td>4</td>
+								<td>5</td>
+								<td>6</td>
+								<td>7</td>
+								<td>8</td>
+								<td>9</td>
+							</tr>
+							<tr>
+								<td>10</td>
+								<td id="today">11</td>
+								<td>12</td>
+								<td>13</td>
+								<td>14</td>
+								<td>15</td>
+								<td>16</td>
+							</tr>
+							<tr>
+								<td>17</td>
+								<td>18</td>
+								<td>19</td>
+								<td>20</td>
+								<td>21</td>
+								<td>22</td>
+								<td>23</td>
+							</tr>
+							<tr>
+								<td>24</td>
+								<td>25</td>
+								<td>26</td>
+								<td>27</td>
+								<td>28</td>
+								<td>29</td>
+								<td>30</td>
+							</tr>
+							<tr>
+								<td>31</td>
+								<td class="pad" colspan="6">&nbsp;</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</li>
+			<li>
+				<h2>Other topics</h2>
+				<ul>
+					<li><a href="#"></a><a href="#">climate</a></li>
+					<li><a href="#">Population</a></li>
+					<li><a href="#"> Effects </a></li>
+				
+					</ul>
+				</li>
+		</ul>
+	</div>
+	<!-- end #sidebar -->
+	<div style="clear: both;">&nbsp;</div>
+</div>
+<!-- end #page -->
+
+<div id="footer">
+	<p>Copyright (c) 2016 Sitename.com. All rights reserved. Design by  Eng :- wajde AL-nawaja .</p>
+</div>
+<!-- end #footer -->
+</div>
+</body>
+</html>
